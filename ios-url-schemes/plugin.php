fa90a11abf77de5fe51d9c1ed133b06efaaa51ee
Matej Sychra <suculent@me.com>
2013-04-08 14:25:12 +0200

    fixed naming conventions
    'starts_with' could be really conflicting, however
    'suculent_starts_with' is equally non-conflicting and fun

--- ios-url-schemes/plugin.php
+++ ios-url-schemes/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: iOS URL Schemes
 Plugin URI: https://github.com/suculent/yourls-ios-url-schemes-plugin
 Description: Support for itms-services URL scheme for linking to iOS Enterprise App Installation Manifest
-Version: 1.2
+Version: 1.3
 Author: Suculent
 Author URI: http://www.github.com/suculent/
 */
@@ -31,7 +31,7 @@ function suculent_itms_protocols( $args, $url ) {
 	
 	/* Walk through the list and check if URL starts with one of them. */
 	foreach ( $protocols as $protocol ) {	
-		if ( starts_with($url, $protocol) === TRUE ) return true;
+		if ( suculent_starts_with($url, $protocol) === TRUE ) return true;
 	}
 	
 	/* None of protocols supported by this plugin has been found in the URL */
@@ -43,9 +43,8 @@ function suculent_itms_protocols( $args, $url ) {
  * Returns true if $haystack starts with $needle
  */
 
-function starts_with($haystack, $needle)
+function suculent_starts_with($haystack, $needle)
 {
     return !strncmp($haystack, $needle, strlen($needle));
 }
-
 ?>
